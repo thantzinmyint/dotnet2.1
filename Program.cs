bb9016ae619c393935532699b36ec09b86373415
Thant <thant_myint@dialog.com.au>
2019-03-15 15:43:35 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
